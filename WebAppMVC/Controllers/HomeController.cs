b43135d63dcce815c1e67e8046722f527c695057
Roberto Borsini <roberto.borsini@gmail.com>
2020-05-04 18:06:40 +0200

    Add project files.

---
+++ WebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            //return RedirectToRoute("About.html");
+            var staticPageToRender = new FilePathResult("~/app/index.html", "text/html");
+            return staticPageToRender;
+            //return View();
+        }
+    }
+}
